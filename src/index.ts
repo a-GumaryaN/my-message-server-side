4199c9680e6b7e3937dfc384886dd3ee28f7f087
a-GumaryaN <alireza313gomarian@gmail.com>
2022-03-12 19:58:14 +0330

    add graphql to the server

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,19 @@
 import Server from "./Server";
+import { userModel } from "./db/models";
 import { graphqlHTTP } from "express-graphql";
+import { graphqlSchema } from "./graphql/graphql";
 const cors = require("cors");
 
 const server = new Server(4000, "mongodb://localhost:27017/cryptex");
 
 server.app.use(cors({ origin: "*" }));
 
+server.app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: graphqlSchema,
+    graphiql: true,
+  })
+);
 
 server.init();
